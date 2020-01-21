a2db7846a78c40a82b86d400d06223e4f42cfa17
Pdarek <227095@student.pwr.edu.pl>
2020-01-21 11:31:47 +0100

    clean genetic

--- genetic.py
+++ genetic.py
@@ -11,3 +11,15 @@ pop_shape = (pop_size, n_features)
 new_population = np.random.randint(2, size=pop_shape)
 print(new_population.shape)
 print(new_population)
+
+
+def reduce_features(solution, features):
+    selected_elements_indices = np.where(solution == 1)[0]
+    reduced_features = features[:, selected_elements_indices]
+    return reduced_features
+
+
+def pop_fitness(X_train, X_test, y_train, y_test, algorithm, population):
+    accuracies = numpy.zeros(population.shape[0])
+    idx = 0
+
