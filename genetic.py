c7bbebc68f5346d6a7e7f4a45c4cf62aa77e4a08
Pdarek <227095@student.pwr.edu.pl>
2020-01-21 11:09:40 +0100

    clean genetic

--- genetic.py
+++ genetic.py
@@ -1,1 +1,13 @@
 import numpy as np
+
+
+n_features = 20
+
+pop_size = 8 # Population size.
+num_parents_mating = 4 # Number of parents inside the mating pool.
+mutations = 3 # Number of elements to mutate.
+
+pop_shape = (pop_size, n_features)
+new_population = np.random.randint(2, size=pop_shape)
+print(new_population.shape)
+print(new_population)
