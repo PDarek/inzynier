28b8c3bd0f8377d200a13c3040cf6221ba9fff30
Pdarek <227095@student.pwr.edu.pl>
2020-01-21 11:49:48 +0100

    clean genetic

--- prog.py
+++ prog.py
@@ -1,4 +1,3 @@
-
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 from sklearn.datasets import make_classification
@@ -20,12 +19,11 @@ n_repeated = 2
 X, y = make_classification(n_samples, n_features, n_informative, n_redundant, n_repeated)
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
 
-
 logreg = LogisticRegression(solver='lbfgs')
 logreg.fit(X_train, y_train)
 
 y_pred = logreg.predict(X_test)
-print('Accuracy of logistic regression classifier on test set: {:.2f}'.format(logreg.score(X_test, y_test)))
+print('Accuracy of logistic regression: {:.2f}'.format(logreg.score(X_test, y_test)))
 confusion_matrix = confusion_matrix(y_test, y_pred)
 print(confusion_matrix)
 print(classification_report(y_test, y_pred))
@@ -42,11 +40,13 @@ plt.plot(fpr, tpr, label='Logistic Regression (area = %0.2f)' % logit_roc_auc)
 plt.plot([0, 1], [0, 1], 'r--')
 plt.xlim([0.0, 1.0])
 plt.ylim([0.0, 1.05])
-plt.xlabel('False Positive Rate')
-plt.ylabel('True Positive Rate')
-plt.title('Receiver operating characteristic')
+plt.xlabel('False Positive')
+plt.ylabel('True Positive')
+plt.title('RO characteristic')
 plt.legend(loc="lower right")
-plt.savefig('Log_ROC')
+plt.savefig('ROC')
 plt.show()
+
 """
+
 print("--- %s seconds ---" % (time.time() - start_time))
